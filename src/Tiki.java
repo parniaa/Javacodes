3a799a0b29d4e0ac7d1fe843f18485cfbb6c8a66
Paarmin <Parniaa@gmail.com>
2018-12-18 22:56:18 -0500

    kkk

---
+++ src/Tiki.java
@@ -0,0 +1,26 @@
+//public class Tiki {
+//
+//    public static void main(String[] args) {
+//        Long startTime = System.currentTimeMillis();
+//        System.out.println("sadasdasdasd");
+//        for (int i=1; i<100;i++){
+//            int a ;
+//           for (int j =0; j<80;j++)
+//
+//               System.out.println("1");
+//
+//        }
+//        Long endTime = System.currentTimeMillis();
+//        Long duration = endTime - startTime;
+//        String armi = "sdasd";
+//        System.out.println(armi.intern());
+//        System.out.println(duration);
+//    }
+//}
+//
+
+public class Tiki {
+    public static void main(String[] args) {
+        System.out.println("h");
+    }
+}
